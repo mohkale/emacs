32089a16db97c8fa35364e4e194bafbd6e6797e7
MoHKale <mohkalsin@gmail.com>
2019-09-12 05:18:57 +0100

    fix indentation and styling for init.org
    also
    * implemented more hydras
    * created some custom snippets
    * add haskell language support
    * add ediff and diff support
    * improve ruby support & add git/magit

--- bin/clean_safe.sh
+++ bin/clean_safe.sh
@@ -5,5 +5,12 @@
 # can rebuild your config from scratch... instead of when
 # you're actually deleting everything... for some reason.
 
+# flag-notes
+#  * never delete any language server installations, 
+#    they're too slow to reinstall for it to be worth it.
+#  * never delete any un-version controlled snippets... 
+#    I'll get round to them... I'm lazy :P.
+
 $(dirname $0)/clean.sh --not --wholename './var/lsp/servers/*'                 \
-                       --not --wholename './var/meghanada/*'
+                       --not --wholename './var/meghanada/*'                   \
+                       --not --wholename './etc/snippets/*'
