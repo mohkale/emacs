97773825fcaa812f6099f03b54d5ab5db5fd122c
MoHKale <mohkalsin@gmail.com>
2020-01-02 19:23:25 +0000

    convert from dotfile repo subfolder to a submodule
    the commits before this point were probably quite messy, I had to
    bundle big changes to init.org alongside minor changes to my dotfiles
    and also didn't have magit to selectively commit hunks.
    
    anyways, this is where my emacs config begins a clean and readable
    history.
    
    also removed bookmarks file because it's a pain to deal with commiting
    changes every time I do an org-capture or whatnot.

---
+++ etc/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote nil))
+ '(display-line-numbers-width nil)
+ '(display-line-numbers-width-start t)
+ '(electric-pair-inhibit-predicate (quote ignore))
+ '(electric-pair-mode t)
+ '(electric-pair-preserve-balance nil)
+ '(electric-pair-skip-whitespace t)
+ '(electric-pair-skip-whitespace-chars nil)
+ '(evil-search-module (quote evil-search))
+ '(helm-descbinds-window-style (quote split-window))
+ '(helm-echo-input-in-header-line t)
+ '(ibuffer-saved-filter-groups nil)
+ '(persp-auto-resume-time -1.0)
+ '(persp-autokill-buffer-on-remove (quote kill-weak))
+ '(persp-init-frame-behaviour nil)
+ '(powerline-default-separator (quote slant))
+ '(safe-local-variable-values (quote ((encoding . utf-8-unix) (encoding . utf-8)))))
